b80a266bb976beffb24d554e61334b5ff9746da0
Alexey Tustik <alexeytystik@gmail.com>
2023-01-31 08:18:02 +0300

    add admin command

--- docker_jupyter_lab/main.py
+++ docker_jupyter_lab/main.py
@@ -3,10 +3,14 @@ import sys
 
 COMMAND = 'docker-compose up -d'
 COMMAND_STOP = 'docker-compose down'
+COMMAND_ADMIN = 'docker exec -it my-jupyter-lab  bash'
 
 if __name__ == '__main__':
     args = sys.argv
+    assert len(sys.argv) > 1, "use commands ['run', 'stop', 'admin']"
     if sys.argv[1] == 'run':
         os.system(COMMAND)
     elif sys.argv[1] == 'stop':
         os.system(COMMAND_STOP)
+    elif sys.argv[1] == 'admin':
+        os.system(COMMAND_ADMIN)
